eea9934bb6344194a565e4885f2823d8ce212da3
gaowei.gao <gaowei.gao@bytedance.com>
2020-05-12 17:11:58 +0800

    first commit
    Signed-off-by: gaowei.gao <gaowei.gao@bytedance.com>

---
+++ noexcept/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+struct A {
+    ~A() {throw 1;}
+};
+
+struct B {
+    ~B() noexcept(false) {throw 2;}
+};
+
+struct C {
+    B b;
+};
+
+int funA() {A a;}
+int funB() {B b;}
+int funC() {C c;}
+
+int main() {
+    try {
+        funB();
+    } catch(...) {
+        cout << "catch funB" << endl;
+    }
+
+    try {
+        funC();
+    } catch(...) {
+        cout << "catch funC" << endl;
+    }
+
+    try {
+        funA();
+    } catch(...) {
+        cout << "catch funA" << endl;
+    }
+    return 0;
+}
